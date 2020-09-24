f0218faddaea4e74f5cf44dce21f3172f5db46f4
Lucía Hurtado Cardoso <57091651+LuciaHurtadoCardoso@users.noreply.github.com>
2020-09-24 20:40:51 +0200

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ListadoComponent } from './listado/listado.component';
+import {Pipe, PipeTransform} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { FilterPipe } from './filter.pipe';
+import { DetalleUserComponent } from './listado/detalle-user/detalle-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoComponent,
+    FilterPipe,
+    DetalleUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
